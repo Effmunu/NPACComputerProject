b4f35badb2df728d3695b5764ad279d0d964cee1
Thibault Chevalérias <tibochev@openmailbox.org>
2016-03-02 12:15:59 +0100

    Enhancing graph display v1

--- Analysis/BetaAlpha_method/plot_alphas.C
+++ Analysis/BetaAlpha_method/plot_alphas.C
@@ -41,8 +41,16 @@ int getMatrixJ(int nbin, int k)
     return k - nbin*(nbin-1)/2 + (nbin-i)*(nbin-i-1)/2;
 }
 
-void compute_alphas(string& type, string& categ, string& nbEvents, int binning, int stained)
+void plot_alphas(string& type, string& categ, string& nbEvents, int binning, int stained)
 {
+    gStyle->SetLegendFillColor(kWhite);
+    gStyle->SetLegendBorderSize(0);
+    gStyle->SetOptTitle(0);
+    gStyle->SetPadTopMargin(0.10);
+    gStyle->SetPadRightMargin(0.10);
+    gStyle->SetPadBottomMargin(0.15);
+    gStyle->SetPadLeftMargin(0.15);
+
     // Prefix for file input
     TString inputPrefix = Form("%s_%s_%s_%d_%s",
         type.c_str(), categ.c_str(), nbEvents.c_str(), binning,
@@ -131,14 +139,35 @@ void compute_alphas(string& type, string& categ, string& nbEvents, int binning, 
         //cout << "x_i: " << x[i] << "; diff_i: " << diff_i[i] << endl;
     }
 
-    TGraph* graph = new TGraph(npar, x, diff_i);
-    TCanvas* canv = new TCanvas("canv", outputPrefix + "_diff_i", 800, 600);
-    graph->Draw("AB");
-    canv->SaveAs("fig/" + outputPrefix + "_diff_i.png", "Q");
+    TPaveText* text_diff = new TPaveText(0.6, 0.7, 0.85, 0.85, "ndc");
+    text_diff->SetBorderSize(0);
+    text_diff->SetTextSize(0.04);
+    text_diff->SetFillColor(kWhite);
+    text_diff->AddText(stained ? "#alpha_{i} - #lambda_{i}" : "#alpha_{i}");
 
-    TGraph* graph2 = new TGraph(npar, x, diff_i_over_sigma_i);
-    TCanvas* canv2 = new TCanvas("canv2",
+    TPaveText* text_diff_over = new TPaveText(0.6, 0.7, 0.85, 0.85, "ndc");
+    text_diff_over->SetBorderSize(0);
+    text_diff_over->SetTextSize(0.04);
+    text_diff_over->SetFillColor(kWhite);
+    text_diff_over->AddText(stained ? "#alpha_{i} - #lambda_{i}" : "#alpha_{i}");
+
+    TGraph* graph_diff = new TGraph(npar, x, diff_i);
+    graph_diff->GetXaxis()->SetTitle("Eta bin Number");
+    graph_diff->GetYaxis()->SetTitle("alpha_i");
+    graph_diff->SetMaximum(graph_diff->GetMaximum()*1.3);
+    TCanvas* canv_diff = new TCanvas("canv_diff",
+        outputPrefix + "_diff_i", 800, 600);
+    graph_diff->Draw("AB");
+    text_diff->Draw();
+    canv_diff->SaveAs("fig/" + outputPrefix + "_diff_i.png", "Q");
+
+    TGraph* graph_diff_over = new TGraph(npar, x, diff_i_over_sigma_i);
+    graph_diff_over->GetXaxis()->SetTitle("Eta bin Number");
+    graph_diff_over->GetYaxis()->SetTitle("alpha_i");
+    graph_diff_over->SetMaximum(graph->GetMaximum()*1.3);
+    TCanvas* canv_diff_over = new TCanvas("canv_diff_over",
         outputPrefix + "_diff_i_over_sigma_i", 800, 600);
-    graph2->Draw("AB");
-    canv2->SaveAs("fig/" + outputPrefix + "_diff_i_over_sigma_i.png", "Q");
+    graph_diff_over->Draw("AB");
+    text_diff_over->Draw();
+    canv_diff_over->SaveAs("fig/" + outputPrefix + "_diff_i_over_sigma_i.png", "Q");
 }
