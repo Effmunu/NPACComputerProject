3e5805103bf345d0046514ad78470fcdefc2df11
Thibault Chevalérias <tibochev@openmailbox.org>
2016-03-02 15:26:52 +0100

    Improved invariant masses plots for BetaAlpha method

--- Analysis/BetaAlpha_method/run.sh
+++ Analysis/BetaAlpha_method/run.sh
@@ -4,7 +4,7 @@ if [ $? -ne 0 ]; then
     echo "error while compiling"
     exit 1
 else
-    for i in `seq 2 2 48`; do
+    for i in `seq 2 2 24`; do
         ./bin/main mc Z 10000 $i 0
     done;
     exit 0
